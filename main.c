6d1b33813993bbb31507560b7d8ba6d9554a97d1
marcloh108 <marc.loh@molisys.com>
2019-11-11 15:10:08 +0700

    Test if sequence 1,2 and 3 is present in array
    Create functions for array sequence and array size
    
    Signed-off-by: marcloh108 <marc.loh@molisys.com>

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    printf("Hello World!\n");
-    return 0;
-}
+int test(int nums[], int array_size)
+       {
+          for (int i = 0; i < array_size-1; i++)
+          {
+              if (nums[i] == 1 && nums[i + 1] == 2 && nums[i + 2] == 3)
+                  return 1;
+          }
+         return 0;
+      }
+
+int main(void)
+    {
+    int array_size;
+    int array1[] = {1,1,2,3,1};
+    array_size = sizeof(array1)/sizeof(array1[0]);
+    printf("%d",test(array1, array_size));
+    int array2[] = {1,1,2,4,1};
+    array_size = sizeof(array2)/sizeof(array2[0]);
+    printf("\n%d",test(array2, array_size));
+    int array3[] = {1,2,2,1,2,3};
+    array_size = sizeof(array3)/sizeof(array3[0]);
+    printf("\n%d",test(array3, array_size));
+    }
